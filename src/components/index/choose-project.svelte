c233fb3e6768c7787d8342381085f8f3a2ecffac
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2021-03-29 20:33:06 +0500

    add strike through text animation.

--- src/components/index/choose-project.svelte
+++ src/components/index/choose-project.svelte
@@ -1,5 +1,38 @@
 <script>
+  import { onMount } from "svelte";
+
   import Section from "../section.svelte";
+
+  onMount(() => {
+    let options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1,
+    };
+
+    let observer = new IntersectionObserver(beTouching, options);
+
+    const target = document.querySelector("#observer-target");
+
+    observer.observe(target);
+
+    function beTouching(entries, ob) {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const target = entry.target;
+          const strikethroughContainer = target.children[1];
+
+          setTimeout(() => {
+            strikethroughContainer.children[0].classList.add("strikethrough");
+
+            setTimeout(() => {
+              strikethroughContainer.children[1].classList.add("strikethrough");
+            }, 700);
+          }, 200);
+        }
+      });
+    }
+  });
 </script>
 
 <style lang="scss">
@@ -21,20 +54,46 @@
     }
   }
 
-  strike {
+  :global(.strikethrough) {
+    display: inline-block;
+    position: relative;
+    transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
     color: var(--light-grey);
+  }
+
+  :global(.strikethrough::after) {
+    content: "";
+    position: absolute;
+    display: block;
+    width: 100%;
+    height: 5px;
+    margin-top: -0.6em;
+    transform-origin: center left;
+    animation: strikethrough 1s 0.5s cubic-bezier(0.55, 0, 0.1, 1) 1 forwards;
+    transition: transform 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+  }
+
+  @keyframes strikethrough {
+    from {
+      transform: scaleX(0);
+      background: var(--light-grey);
+    }
+    to {
+      transform: scaleX(1);
+      background: var(--light-grey);
+    }
   }
 </style>
 
 <Section>
   <div class="row">
-    <h1>
+    <h2 class="h2" id="observer-target">
       Choose project,
       <br />
-      <strike>
-        check dependencies, checkout branch, view readme.txt, install tools, run
-        build, run test,
-      </strike>
+      <span>
+        <span>check dependencies, checkout branch, view</span>
+        <span>readme.txt, install tools, run build, run test,</span>
+      </span>
       <br />
       start coding.
     </h1>
