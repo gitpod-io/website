c268f238aee626853aa99961a260d129536a1543
Mike Nikles <mikenikles@gmail.com>
2021-04-23 03:05:58 +0000

    Use HTML content for individual changelog items for now.

--- src/routes/changelog/rss.xml.ts
+++ src/routes/changelog/rss.xml.ts
@@ -1,17 +1,18 @@
-import Turndown from "turndown";
-
 export const get: import("@sveltejs/kit").RequestHandler = ({ context }) => {
-  const turndownService = new Turndown();
   const ttlInMin = 60;
   const rssDocument = `<?xml version="1.0"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
-   <title>RSS Title</title>
-   <description>This is an example of an RSS feed</description>
-   <link>http://www.example.com/main.html</link>
-   <copyright>2020 Example.com All rights reserved</copyright>
-   <lastBuildDate>Mon, 06 Sep 2010 00:01:00 +0000 </lastBuildDate>
-   <pubDate>Sun, 06 Sep 2009 16:20:00 +0000</pubDate>
+   <title>Gitpod Changelog</title>
+   <description>New features and improvements to Gitpod.</description>
+   <link>https://www.gitpod.io/changelog</link>
+   <copyright>${new Date().getFullYear()} Gitpod GmbH. All rights reserved</copyright>
+   <lastBuildDate>${new Date(
+     context.changelogEntries[0].date
+   ).toUTCString()}</lastBuildDate>
+   <pubDate>${new Date(
+     context.changelogEntries[0].date
+   ).toUTCString()}</pubDate>
    <ttl>${ttlInMin}</ttl>
    <atom:link href="https://www.gitpod.io/changelog/rss.xml" rel="self" type="application/rss+xml" />
    ${context.changelogEntries.map(
@@ -23,8 +24,7 @@ export const get: import("@sveltejs/kit").RequestHandler = ({ context }) => {
       })}</title>
       <link>https://www.gitpod.io/changelog</link>
       <pubDate>${new Date(Date.parse(entry.date)).toUTCString()}</pubDate>
-      <guid isPermaLink="false">7bd204c6-1655-4c27-aeee-53f933c5395f</guid>
-      <description>${turndownService.turndown(entry.content)}</description>
+      <description><![CDATA['${entry.content}']]></description>
     </item>`
    )}
   </channel>
