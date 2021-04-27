ff0ab4821be8c9b5ba4985d5f752cde885a9f9c8
nisarhassan12 <syednisarhassan12@gmail.com>
2021-04-27 23:32:29 +0500

    create header github student developer pack.

---
+++ src/routes/github-student-developer-pack.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Header from "../components/github-student-developer-pack/header.svelte";
+  import OpenGraph from "../components/open-graph.svelte";
+</script>
+
+<style>
+</style>
+
+<OpenGraph
+  data={{
+    title: "GitHub Student Developer Pack",
+    description:
+      "With the GitHub Student Developer Pack, you get the same features as with our usual plans but at a much better price. We’re happy to be able to empower student developers participating in it.",
+  }}
+/>
+
+<Header />
