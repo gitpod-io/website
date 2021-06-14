1023b1f14550e82f606f1c3f4e9f993fcbac076a
nisarhassan12 <syednisarhassan12@gmail.com>
2021-06-15 00:45:14 +0500

    don't show the arrow if the image is the only child of an anchor.

--- src/utils/remark-link-with-image-as-only-child.cjs
+++ src/utils/remark-link-with-image-as-only-child.cjs
@@ -3,12 +3,12 @@ const visit = require("unist-util-visit");
 const visitor = (node) => {
   node.data = node.data || {};
   node.data.hProperties = node.data.hProperties || {};
-  if (
-    node.type === "link" &&
-    node.children[0].type === "image" &&
-    node.children.length === 1
-  ) {
-    node.data.hProperties.class = "link-with-image-as-only-child";
+  if (node.type === "link") {
+    if (node.children && node.children.length && node.children.length === 1) {
+      if (node.children[0].type === "image") {
+        node.data.hProperties.class = "link-with-image-as-only-child";
+      }
+    }
   }
 };
 
