55283ac3134c231d5f8d16b17dca3267f1af2c04
nisarhassan12 <syednisarhassan12@gmail.com>
2021-06-28 20:31:31 +0500

    add the ui for the newsletter signup widget.

---
+++ src/components/blog/newsletter-signup.svelte
@@ -0,0 +1,27 @@
+<style type="text/postcss">
+  input {
+    @apply mb-0;
+  }
+
+  button {
+    @apply mr-0 w-32;
+  }
+</style>
+
+<div class="mt-huge mb-xx-large">
+  <form
+    class="bg-white shadow-normal rounded-2xl max-w-2xl py-small px-xx-small sm:px-small md:px-x-large m-auto"
+  >
+    <h2>Stay updated</h2>
+    <p class="text-large">Sign up now for our newsletter.</p>
+    <div class="flex mt-x-small">
+      <input
+        type="email"
+        placeholder="Enter your email"
+        autocomplete="email"
+        class="mr-macro sm:mr-xx-small"
+      />
+      <button class="btn-primary" type="submit">Sign up</button>
+    </div>
+  </form>
+</div>
