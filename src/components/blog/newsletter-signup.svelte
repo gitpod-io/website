40ac283f5265d3ebc0c135234d89373fb635919e
nisarhassan12 <syednisarhassan12@gmail.com>
2021-06-29 21:30:29 +0500

    small adjustments + address the feedback on the Pr.

--- src/components/blog/newsletter-signup.svelte
+++ src/components/blog/newsletter-signup.svelte
@@ -4,13 +4,22 @@
   };
 </script>
 
+<style type="text/postcss">
+  input {
+    @apply mb-0;
+  }
+  button {
+    @apply mr-0 w-32;
+  }
+</style>
+
 <div class="mt-large my-xx-large">
   <form
-    class="bg-white shadow-normal rounded-2xl max-w-2xl py-small px-xx-small sm:px-small md:px-x-large m-auto"
+    class="bg-white shadow-normal rounded-2xl max-w-lg py-small px-xx-small sm:px-small m-auto"
     on:submit|preventDefault={submitEmail}
   >
-    <h2>Stay updated</h2>
-    <p class="text-large">Sign up now for our newsletter.</p>
+    <h2 class="h3">Stay updated</h2>
+    <p class="text-medium">Sign up now for our newsletter.</p>
     <div class="flex mt-x-small">
       <input
         type="email"
