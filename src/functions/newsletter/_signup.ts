3c8982dbbbe6a86f2a4db747e4b809cf7fc09236
nisarhassan12 <syednisarhassan12@gmail.com>
2021-07-02 00:03:07 +0500

    remove the un necessary code.

--- src/functions/newsletter/_signup.ts
+++ src/functions/newsletter/_signup.ts
@@ -2,15 +2,14 @@ import type { Response } from "@netlify/functions/src/function/response";
 import saveEmailInSheet from "../feedback/_save-to-spreadsheet";
 
 export const signup = async (body: string): Promise<Response> => {
-  const email = JSON.parse(body);
   const isSavedInSheet = await saveEmailInSheet({
     sheetTitle: "Newsletter - Signups",
-    data: [email],
+    data: [body],
   });
 
   const statusCode = isSavedInSheet ? 201 : 500;
   return {
     statusCode,
-    body: statusCode === 201 ? "Signed up" : "Oh no, something failed." + email,
+    body: statusCode === 201 ? "Signed up" : "Oh no, something failed.",
   };
 };
--- src/functions/newsletter/index.ts
+++ src/functions/newsletter/index.ts
@@ -2,8 +2,6 @@ import type { Handler } from "@netlify/functions";
 import { signup } from "./_signup";
 
 const handler: Handler = (event, _, callback) => {
-  console.log(JSON.stringify(event.body));
-
   signup(event.body)
     .then((response) => callback(null, response))
     .catch((error) => {
