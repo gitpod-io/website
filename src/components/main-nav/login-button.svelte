9a87afae7af46783f40d486503d98687bf8f4704
Mike Nikles <mikenikles@gmail.com>
2021-07-02 18:40:33 +0000

    Use font-bold for the login button.

--- src/components/main-nav/login-button.svelte
+++ src/components/main-nav/login-button.svelte
@@ -1,6 +1,6 @@
 <a
   href="https://gitpod.io/login/"
-  class="flex items-center justify-center h-8 w-20 rounded-xl bg-black text-off-white text-sm focus:text-off-white focus:bg-black-hover hover:text-off-white hover:bg-black-hover"
+  class="flex items-center justify-center h-8 w-20 rounded-xl bg-black font-bold text-off-white text-sm focus:text-off-white focus:bg-black-hover hover:text-off-white hover:bg-black-hover"
 >
   Login
 </a>
