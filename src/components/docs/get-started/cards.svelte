d5cc12f4ff6090d7f61f0ab17baf80f48cd4d5a5
Derya A. Antonelli <derya.antonelli@gmail.com>
2021-07-09 20:13:57 +0000

    Commit cards component #732

---
+++ src/components/docs/get-started/cards.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Card from "./card.svelte";
+  import type { ContentType } from "../../../types/get-started-card-content.type";
+  //import GetStarted from "./get-started.svelte";
+
+  export let cardContents: ContentType[];
+</script>
+
+<div class="card-container flex flex-wrap align-middle">
+  {#each cardContents as content}
+    <Card {content} />
+  {/each}
+</div>
