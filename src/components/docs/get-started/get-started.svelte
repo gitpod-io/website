c79b50c85ebfcee19627783ea90d44fed0d85215
Derya A. Antonelli <derya.antonelli@gmail.com>
2021-07-09 20:15:05 +0000

    Commit get started section #732

---
+++ src/components/docs/get-started/get-started.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { cardContents } from "../../../contents/get-started";
+  import Cards from "./cards.svelte";
+</script>
+
+<h2>Choose your learning style</h2>
+<div class="card-container flex flex-wrap align-middle">
+  <Cards {cardContents} />
+</div>
