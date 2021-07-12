0ac273533cdec8975ae44ed5d01280841851db1b
Derya A. Antonelli <derya.antonelli@gmail.com>
2021-07-12 15:08:26 +0000

    Remove unused comment #732

--- src/components/docs/get-started/cards.svelte
+++ src/components/docs/get-started/cards.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import Card from "./card.svelte";
   import type { ContentType } from "../../../types/get-started-card-content.type";
-  //import GetStarted from "./get-started.svelte";
 
   export let cardContents: ContentType[];
 </script>
