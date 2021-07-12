b444f6ec7de427283b0b1d98e46e7f9a77c95468
Derya A. Antonelli <derya.antonelli@gmail.com>
2021-07-12 14:14:44 +0000

    Modify heading for accessibility #732

--- src/components/docs/get-started/get-started.svelte
+++ src/components/docs/get-started/get-started.svelte
@@ -3,10 +3,10 @@
   import Cards from "./cards.svelte";
 </script>
 
-<h2>Select your learning style</h2>
-<h5 class="text-dark-grey mb-10">
+<h2 class="mt-medium">Select your learning style</h2>
+<p class="mb-10 text-large">
   Get started with Gitpod the way that best matches your learning style.
-</h5>
+</p>
 <div class="card-container flex flex-wrap align-middle">
   <Cards {cardContents} />
 </div>
