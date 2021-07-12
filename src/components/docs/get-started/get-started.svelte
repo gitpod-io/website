3aeff79fe2cccaf6fff296155dda06becd35a360
Derya A. Antonelli <derya.antonelli@gmail.com>
2021-07-12 11:05:25 +0000

    Add sub-header #732

--- src/components/docs/get-started/get-started.svelte
+++ src/components/docs/get-started/get-started.svelte
@@ -3,7 +3,10 @@
   import Cards from "./cards.svelte";
 </script>
 
-<h2>Choose your learning style</h2>
+<h2>Select your learning style</h2>
+<h5 class="text-dark-grey mb-10">
+  Get started with Gitpod the way that best matches your learning style.
+</h5>
 <div class="card-container flex flex-wrap align-middle">
   <Cards {cardContents} />
 </div>
