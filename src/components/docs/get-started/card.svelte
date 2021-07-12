420ae4e5fe59a49fa03766f8fe542bb06e20e858
Derya A. Antonelli <derya.antonelli@gmail.com>
2021-07-12 14:13:05 +0000

    Make cards responsive #732

--- src/components/docs/get-started/card.svelte
+++ src/components/docs/get-started/card.svelte
@@ -6,13 +6,13 @@
 </script>
 
 <div
-  class="text-center w-72 bg-off-white rounded-2xl py-xx-small px-x-small shadow-normal mr-micro mb-x-small flex flex-col"
+  class="text-center w-full lg:w-72 bg-off-white rounded-2xl pb-xx-small pt-x-small px-5 shadow-normal mr-micro mb-x-small flex flex-col items-center"
 >
-  <img src={imgSrc} alt={imgAlt} />
-  <div class="flex items-start justify-center h-14 mt-10 mb-4">
+  <img src={imgSrc} alt={imgAlt} width="173" height="112" />
+  <div class="flex items-start justify-center lg:h-14 mt-5 lg:mt-10 mb-4">
     <h5 class="leading-normal">{title}</h5>
   </div>
-  <div class="flex items-start justify-center h-24 mb-5 overflow-hidden">
+  <div class="flex items-start justify-center lg:h-24 mb-5 overflow-hidden">
     <p>{description}</p>
   </div>
   <a href={btnHref} class="btn-conversion">{btnText}</a>
